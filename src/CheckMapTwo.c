184bab86463b8e56fdaa601d53cb5c959f55ba98
Mohamed Haddaoui <mhaddaou@e2r10p12.1337.ma>
2022-09-17 01:11:41 +0100

    fix short name textures

--- src/CheckMapTwo.c
+++ src/CheckMapTwo.c
@@ -6,7 +6,7 @@
 /*   By: mhaddaou <mhaddaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 09:41:49 by mhaddaou          #+#    #+#             */
-/*   Updated: 2022/09/17 00:08:48 by mhaddaou         ###   ########.fr       */
+/*   Updated: 2022/09/17 01:10:26 by mhaddaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,17 @@ int	check_empty_line(char *line)
 
 void	check_short_name_two(t_map *map, char *str)
 {
-	if (!ft_strncmp(str, "EA", ft_strlen(str)))
+	if (!ft_strncmp(str, "EA", 3))
 	{
 		get_info(map, 'E');
 		map->e++;
 	}
-	else if (!ft_strncmp(str, "F", ft_strlen(str)))
+	else if (!ft_strncmp(str, "F", 2))
 	{
 		get_info(map, 'F');
 		map->f++;
 	}
-	else if (!ft_strncmp(str, "C", ft_strlen(str)))
+	else if (!ft_strncmp(str, "C", 2))
 	{
 		get_info(map, 'C');
 		map->c++;
@@ -44,17 +44,17 @@ void	check_short_name(t_map *map, char *str, int i, int j)
 {
 	map->i = i;
 	map->j = j;
-	if (!ft_strncmp(str, "NO", ft_strlen(str)))
+	if (!ft_strncmp(str, "NO", 3))
 	{
 		get_info(map, 'N');
 		map->n++;
 	}
-	else if (!ft_strncmp(str, "SO", ft_strlen(str)))
+	else if (!ft_strncmp(str, "SO", 3))
 	{
 		get_info(map, 'S');
 		map->s++;
 	}
-	else if (!ft_strncmp(str, "WE", ft_strlen(str)))
+	else if (!ft_strncmp(str, "WE", 3))
 	{
 		get_info(map, 'W');
 		map->w++;
